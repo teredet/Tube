dfbd3551f16e08ea4135580aeb1c28c77683410c
teredet <artem.vanzytler@gmail.com>
2021-06-16 13:04:17 +0300

    Make main page, some models and main view

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+
+    name = models.CharField(max_length=255)
+
+
+class Video(models.Model):
+
+    title = models.CharField(max_length=255)
+    file = models.FileField()
+
+
+class VideoTag(models.Model):
+
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+from .models import Video
+
+
+class MainView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        videos = Video.objects.all()
+        video_count = Video.objects.all().count()
+        context = {
+            'videos': videos,
+            'video_count': video_count,
+        }
+
+        return render(request, 'main/main.html', context)
